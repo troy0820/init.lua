fc7b292dd73c38ae787260266e4af68806e5f9bc
Troy Connor <troy0820@users.noreply.github.com>
2024-12-11 15:36:01 -0500

    update telescope
    Signed-off-by: Troy Connor <troy0820@users.noreply.github.com>

--- lua/troy0820/lazy/lsp.lua
+++ lua/troy0820/lazy/lsp.lua
@@ -1,47 +1,46 @@
 return {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  dependencies = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
 
-		  {'neovim/nvim-lspconfig'},
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lsp-signature-help'},
-		  {'L3MON4D3/LuaSnip'},
-	  },
-config = function()
-	local lsp_zero = require('lsp-zero')
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+  },
+  config = function()
+    local lsp_zero = require('lsp-zero')
 
-	lsp_zero.on_attach(function(client, bufnr)
-		lsp_zero.default_keymaps({buffer = bufnr})
-	end)
+    lsp_zero.on_attach(function(client, bufnr)
+      lsp_zero.default_keymaps({ buffer = bufnr })
+    end)
 
-	require('mason').setup({})
-	require('mason-lspconfig').setup({
-		handlers = {
-			function(server_name)
-				require('lspconfig')[server_name].setup({}) --lsp_zero.default_setup,
-			end
-		},
-	})
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({}) --lsp_zero.default_setup,
+        end
+      },
+    })
 
-	local cmp = require('cmp')
-	local cmp_action = require('lsp-zero').cmp_action()
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
 
-	cmp.setup({
-		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'nvim_lsp_signature_help' }
+    cmp.setup({
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' }
 
-		}),
-		mapping = cmp.mapping.preset.insert({
-			['<Tab>'] = cmp_action.tab_complete(),
-			['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-		}),
-	})
-end
+      }),
+      mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+      }),
+    })
+  end
 }
--- lua/troy0820/lazy/telescope.lua
+++ lua/troy0820/lazy/telescope.lua
@@ -1,14 +1,25 @@
 return {
 
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-  dependencies = {"nvim-lua/plenary.nvim"},
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  },
 
   config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>ag', function ()
-          builtin.grep_string({ search = vim.fn.input("Grep > " ) });
-end)
+    require('telescope').setup {
+      extensions = {
+        fzf = {}
+      },
+    }
+    require('telescope').load_extension('fzf')
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ag', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    end)
   end
 }
